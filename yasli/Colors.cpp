e3b98f4baff1b3b0e8722486f46e8509666dae60
Evgeny Andreeshchev <koalefant@fastmail.com>
2009-06-26 13:41:46 +0300

    missing colors serialization

---
+++ yasli/Colors.cpp
@@ -0,0 +1,27 @@
+#include "StdAfx.h"
+#include "Colors.h"
+#include "yasli/Archive.h"
+
+void Color4f::serialize(Archive& ar)
+{
+    ar(r, "", "R");
+    ar(g, "", "G");
+    ar(b, "", "B");
+    ar(a, "", "A");
+}
+
+void Color3c::serialize(Archive& ar)
+{
+    ar(r, "", "R");
+    ar(g, "", "G");
+    ar(b, "", "B");
+}
+
+void Color4c::serialize(Archive& ar)
+{
+    ar(r, "", "R");
+    ar(g, "", "G");
+    ar(b, "", "B");
+    ar(a, "", "A");
+}
+
