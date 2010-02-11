59bec49b28765beb49c330eb769a3c34ab83813d
Evgeny Andreeshchev <koalefant@fastmail.com>
2010-02-12 00:05:01 +0200

    removed obsolete property-tree + PropertyExample

--- property-tree/PropertyEdit.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace yasli{
-	class Serializer;
-}
-
-void propertyEdit(yasli::Serializer& serializer, int options);
--- property-tree/wxSerialization.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class Archive;
-class wxRect;
-class wxWindow;
-
-bool serialize(Archive& ar, wxRect& rect, const char* name);
-bool serialize(Archive& ar, wxWindow& window, const char* name);
