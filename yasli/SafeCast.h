b8fb77df61892e0d38dcf559430fb89bc4ec86ab
Alexander Kotliar <AlexanderKotliar@gmail.com>
2010-09-21 17:27:26 +0300

    safe_cast for references

--- yasli/SafeCast.h
+++ yasli/SafeCast.h
@@ -8,13 +8,22 @@ namespace yasli{
 #pragma warning (disable:4127)
 
 template<class _Dest, class _Source>
-_Dest safe_cast(_Source source){
+_Dest safe_cast(_Source* source){
 #ifdef _DEBUG
-    _Dest result = dynamic_cast<_Dest>(source);
-    ASSERT(!source || result && "safe_cast failed!");
-    return result;
+	_Dest result = dynamic_cast<_Dest>(source);
+	ASSERT(!source || result && "safe_cast failed!");
+	return result;
 #else
-    return static_cast<_Dest>(source);
+	return static_cast<_Dest>(source);
+#endif
+}
+
+template<class _Dest, class _Source>
+_Dest safe_cast(_Source& source){
+#ifdef _DEBUG
+	return dynamic_cast<_Dest>(source);
+#else
+	return static_cast<_Dest>(source);
 #endif
 }
 
