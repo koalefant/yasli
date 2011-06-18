d1cb3b7529059b392cd32490adec41b9f1b0bbdf
Evgeny Andreeshchev <koalefant@fastmail.com>
2011-06-18 18:09:30 +0200

    ww/Macros.h

---
+++ ww/Macros.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "yasli/Assert.h"
+
+#define FOR_EACH(container, it) for( it = (container).begin(); it != (container).end(); ++it )
+
+template<class T, size_t Len>
+char (&globalArrayLenHelper(const T(&)[Len]))[Len];
+#define ARRAY_LEN(arr) sizeof(globalArrayLenHelper(arr)) 
+
+#ifndef NDEBUG
+# define WW_VERIFY(x) ASSERT(x)
+#else
+# define WW_VERIFY(x) (x)
+#endif
+
+#undef min
+#undef max
+
+inline int min(int x,int y){ return x < y ? x : y; }
+inline float min(float x,float y){ return x < y ? x : y; }
+inline double min(double x,double y){ return x < y ? x : y; }
+
+inline int max(int x,int y){ return x > y ? x : y; }
+inline float max(float x,float y){ return x > y ? x : y; }
+inline double max(double x,double y){ return x > y ? x : y; }
