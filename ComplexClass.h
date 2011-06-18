6a10c95cd4099bde86f89c4412b57ef9032774cd
Evgeny Andreeshchev <koalefant@fastmail.com>
2011-06-18 17:18:56 +0200

    fixed warning in test code

--- ComplexClass.h
+++ ComplexClass.h
@@ -12,6 +12,10 @@ using namespace yasli;
 
 #define YCHECK(x) ASSERT(x); CHECK(x)
 
+#ifdef _MSC_VER
+# pragma warning(disable: 4146) // unary minus operator applied to unsigned type, result still unsigned
+#endif
+
 struct Member
 {
 	std::string name;
