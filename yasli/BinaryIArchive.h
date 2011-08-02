fcde9a1ef7ab7d73e08fc238752fcecb722fd738
Evgeny Andreeshchev <koalefant@fastmail.com>
2011-08-02 23:35:04 +0200

    comments

--- yasli/BinaryIArchive.h
+++ yasli/BinaryIArchive.h
@@ -1,3 +1,12 @@
+/**
+ *  yasli - Serialization Library.
+ *  Copyright (C) 2007-2011 Evgeny Andreeshchev <eugene.andreeshchev@gmail.com>
+ *                          Alexander Kotliar <alexander.kotliar@gmail.com>
+ * 
+ *  This code is distributed under the MIT License:
+ *                          http://www.opensource.org/licenses/MIT
+ */
+
 #pragma once
 #include "Archive.h"
 #include "BinaryNode.h"
