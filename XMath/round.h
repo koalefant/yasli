c079559fb2e27c0ce1042fff6a8b3a19ed70674a
Alexander Kotliar <AlexanderKotliar@gmail.com>
2012-02-07 16:33:16 +0300

    round by sse

--- XMath/round.h
+++ XMath/round.h
@@ -1,8 +1,5 @@
 #pragma once
-
-#ifdef _WIN64
 #include <emmintrin.h>
-#endif
 
 #pragma warning (disable : 4244)
 
@@ -18,29 +15,20 @@ inline T clamp(const T& x, const T1& xmin, const T2& xmax)
 
 inline int round(double x)
 {
-    int a;
-#ifdef _WIN64
+#ifdef _M_AMD64
 	return _mm_cvtsd_si32(_mm_load_sd(&x));
 #else
-    _asm {
+	int a;
+	_asm {
         fld x
         fistp dword ptr a
     }
+	return a;
 #endif
-    return a;
 }
 
 inline int round(float x)
 {
-#ifdef _WIN64
 	return _mm_cvtt_ss2si(_mm_load_ss(&x));
-#else
-    int a;
-    _asm {
-        fld x
-        fistp dword ptr a
-    }
-    return a;
-#endif
 }
 
