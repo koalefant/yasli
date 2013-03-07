30e1857d4c0fdfdb72f5304bb182ba4fd53a9f95
Evgeny Andreeshchev <koalefant@fastmail.com>
2013-03-07 09:07:59 +0100

    missing header

---
+++ yasli/KeyValue.h
@@ -0,0 +1,14 @@
+namespace yasli {
+
+class Archive;
+
+class KeyValueInterface
+{
+public:
+	virtual const char* getKey() = 0;
+	virtual void setKey(const char* key) = 0;
+	virtual bool serializeValue(Archive& ar) = 0;
+};
+
+}
+
