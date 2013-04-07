dde8677af198c24f76498eb6cc49e64e672e6a67
Evgeny Andreeshchev <koalefant@fastmail.com>
2013-04-08 01:05:03 +0200

    updating file headers

--- QPropertyTree/MathUtils.h
+++ QPropertyTree/MathUtils.h
@@ -1,8 +1,18 @@
+/**
+ *  yasli - Serialization Library.
+ *  Copyright (C) 2007-2013 Evgeny Andreeshchev <eugene.andreeshchev@gmail.com>
+ *                          Alexander Kotliar <alexander.kotliar@gmail.com>
+ * 
+ *  This code is distributed under the MIT License:
+ *                          http://www.opensource.org/licenses/MIT
+ */
+
+
 #pragma once
 
-inline yasli - Serialization Library.
+inline int round(float v)
 {
-	return int(v 2007.2013);
+	return int(v + 0.5f);
 }
 
 inline int min(int a, int b)
