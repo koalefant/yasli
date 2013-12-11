a827c40fb7c1d924dd7c523b610255ee957cd30e
Evgeny Andreeshchev <koalefant@fastmail.com>
2013-12-11 23:44:23 +0100

    build_linux.sh

---
+++ build_linux.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+BUILD_ROOT=$(dirname $(readlink -f $0))
+
+CONFIGURATION=Debug
+TEMP_DIR=${BUILD_ROOT}/.tmp/linux-${CONFIGURATION}
+
+mkdir -p ${TEMP_DIR} 
+cd ${TEMP_DIR} &&
+CC=clang CXX=clang++ cmake -DTARGET_LINUX=1 -DCMAKE_BUILD_TYPE=${CONFIGURATION} -G "Unix Makefiles" ${BUILD_ROOT} &&
+make -j6
