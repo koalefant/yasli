6200e87cb5ad256ceb9fed6b51fd7e2f1da748a5
Evgeny Andreeshchev <koalefant@fastmail.com>
2015-03-22 21:32:00 +0100

    build_linux.sh: enabled CMAKE_EXPORT_COMPILE_COMMANDS by default (for YouCompleteMe)

--- build_linux.sh
+++ build_linux.sh
@@ -7,5 +7,9 @@ TEMP_DIR=${BUILD_ROOT}/.tmp/linux-${CONFIGURATION}
 
 mkdir -p ${TEMP_DIR} 
 cd ${TEMP_DIR} &&
-CC=clang CXX=clang++ cmake -DTARGET_LINUX=1 -DCMAKE_BUILD_TYPE=${CONFIGURATION} -G "Unix Makefiles" ${BUILD_ROOT} &&
+CC=clang CXX=clang++ cmake \
+	-DTARGET_LINUX=1 \
+	-DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+	-DCMAKE_BUILD_TYPE=${CONFIGURATION} \
+	-G "Unix Makefiles" ${BUILD_ROOT} &&
 make -j6
