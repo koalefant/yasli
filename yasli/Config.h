eb77395a409d50b6a9ee4a0b719b38f52fed57d9
Evgeny Andreeshchev <koalefant@fastmail.com>
2015-06-28 17:53:09 +0200

    fixed int typedefs on msvc

--- yasli/Config.h
+++ yasli/Config.h
@@ -59,6 +59,19 @@
 // Allows to override default integer typedefs
 // See note at CastInteger in Archive.h for details.
 #ifndef YASLI_INTS_DEFINED
+#ifdef _MSC_VER
+namespace yasli
+{
+	typedef signed __int8 i8;
+	typedef signed __int16 i16;
+	typedef signed __int32 i32;
+	typedef signed __int64 i64;
+	typedef unsigned __int8 u8;
+	typedef unsigned __int16 u16;
+	typedef unsigned __int32 u32;
+	typedef unsigned __int64 u64;
+};
+#else
 #include <stdint.h>
 namespace yasli
 {
@@ -71,6 +84,7 @@ namespace yasli
 	typedef uint32_t u32;
 	typedef uint64_t u64;
 };
+#endif
 #endif
 
 // Allows to override default string implementation (should be STL-compatible)
