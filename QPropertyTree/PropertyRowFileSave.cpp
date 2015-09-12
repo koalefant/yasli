e721f6b64d2a658a64073ac51b1085dea03e14be
Evgeny Andreeshchev <koalefant@fastmail.com>
2015-09-13 01:49:59 +0200

    QPropertyTree: fixed bubbles next to FileSave decorator

--- QPropertyTree/PropertyRowFileSave.cpp
+++ QPropertyTree/PropertyRowFileSave.cpp
@@ -24,37 +24,53 @@
 
 using yasli::FileSave;
 
-class PropertyRowFileSave : public PropertyRowImpl<FileSave>{
+class PropertyRowFileSave : public PropertyRowField{
 public:
 
+	bool assignTo(const Serializer& ser) const override{
+		*ser.cast<FileSave>() = value_;
+		return true;
+	}
 	bool isLeaf() const override{ return true; }
+	bool isStatic() const override{ return false; }
+
+	void setValueAndContext(const Serializer& ser, Archive& ar) override {
+		FileSave* value = ser.cast<FileSave>();
+		value_ = *value;
+		searchHandle_ = value->pathPointer;
+	}
+
+	void serializeValue(yasli::Archive& ar) override{
+		ar(value_, "value", "Value");
+	}
+	WidgetPlacement widgetPlacement() const override{ return WIDGET_VALUE; }
 
 	bool onActivate(const PropertyActivationEvent& e) override
 	{
 		QFileDialog dialog(e.tree->ui()->qwidget());
 		dialog.setAcceptMode(QFileDialog::AcceptSave);
 		dialog.setFileMode(QFileDialog::AnyFile);
-		dialog.setDefaultSuffix(extractExtensionFromFilter(value().filter.c_str()).c_str());
+		dialog.setDefaultSuffix(extractExtensionFromFilter(value_.filter.c_str()).c_str());
 		if (labelUndecorated())
 			dialog.setWindowTitle(QString("Choose file for '") + labelUndecorated() + "'");		
 		
-		QDir directory(value().relativeToFolder.c_str());
-		dialog.setNameFilter(value().filter.c_str());
+		QDir directory(value_.relativeToFolder.c_str());
+		dialog.setNameFilter(value_.filter.c_str());
 		
-		QString existingFile = value().path.c_str();
+		QString existingFile = value_.path.c_str();
 		if (!QDir::isAbsolutePath(existingFile))
 			existingFile = directory.currentPath() + QDir::separator() + existingFile;
 
 		if (!QFile::exists(existingFile))
 			dialog.setDirectory(directory);
 		else
-			dialog.selectFile(QString(value().path.c_str()));
+			dialog.selectFile(QString(value_.path.c_str()));
 
 		if (dialog.exec() && !dialog.selectedFiles().isEmpty()) {
 			e.tree->model()->rowAboutToBeChanged(this);
 			QString filename = dialog.selectedFiles()[0];
 			QString relativeFilename = directory.relativeFilePath(filename);
-			value().path = relativeFilename.toLocal8Bit().data();
+			value_.path = relativeFilename.toLocal8Bit().data();
 			e.tree->model()->rowChanged(this);
 		}
 		return true;
@@ -68,7 +84,12 @@ public:
 	}
 	virtual bool usePathEllipsis() const override{ return true; }
 
-	yasli::string valueAsString() const override{ return value().path; }
+	yasli::string valueAsString() const override{ return value_.path; }
+	const void* searchHandle() const override { return searchHandle_; }
+	yasli::TypeID typeId() const override { return yasli::TypeID::get<yasli::string>(); }
+private:
+	FileSave value_;
+	const void* searchHandle_;
 };
 
 REGISTER_PROPERTY_ROW(FileSave, PropertyRowFileSave); 
