dea7f18ecb20cbcbe1b72b00a47c65babe87840e
Alexander Kotliar <AlexanderKotliar@gmail.com>
2015-12-04 16:50:59 +0200

    xmath: interactiveExceptionHandler

--- XMath/exception.h
+++ XMath/exception.h
@@ -3,4 +3,5 @@
 void setExceptionMessage(const char* msg);
 void setMiniDampName(const char* name);
 void setExceptionHandler(void (*pf)());
+void setInteractiveExceptionHandler(bool interactiveEH);
 
