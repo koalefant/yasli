9dc0bda91ca326408554adfb9e411912789cdec1
Evgeny Andreeshchev <koalefant@fastmail.com>
2016-05-30 10:55:15 +0200

    using Ninja instead of Make

--- build_linux.sh
+++ build_linux.sh
@@ -8,11 +8,11 @@ if [ "$CONFIGURATION" = "" ]; then
 fi
 TEMP_DIR=${BUILD_ROOT}/.tmp/linux-${CONFIGURATION}
 
+echo "make: Entering Directory '${TEMP_DIR}'"
 mkdir -p ${TEMP_DIR} 
 cd ${TEMP_DIR} &&
 CC=clang CXX=clang++ cmake \
-	-DTARGET_LINUX=1 \
 	-DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
 	-DCMAKE_BUILD_TYPE=${CONFIGURATION} \
-	-G "Unix Makefiles" ${BUILD_ROOT} &&
-make -j6
+	-G "Ninja" ${BUILD_ROOT} &&
+ASAN_OPTIONS="detect_leaks=0" ninja
