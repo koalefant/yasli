8eff5ef2d52dd7d8c13c2995e924b182a917265e
Evgeny Andreeshchev <koalefant@fastmail.com>
2017-04-17 13:32:53 +0200

    removed VS projects in favor of CMake

--- build_windows_all.cmd
+++ build_windows_all.cmd
@@ -2,12 +2,9 @@
 setlocal
 
 cd /d %~d0%~p0
-
 mkdir .tmp-vc2013
 cd .tmp-vc2013
-
 set CMAKE="c:\Program Files\CMake\bin\cmake.exe"
-
 %CMAKE% .. ^
 	-DYASLI_NO_QT=1 ^
 	-G"Visual Studio 12 2013" ^
@@ -15,6 +12,6 @@ set CMAKE="c:\Program Files\CMake\bin\cmake.exe"
 
 set DEVENV2013="c:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com"
 %DEVENV2013% yasli.sln /Build Debug   2>&1 || ( exit /b -1 )
-:: %DEVENV2013% yasli.sln /Build Release 2>&1 || ( exit /b -1 )
+%DEVENV2013% yasli.sln /Build Release 2>&1 || ( exit /b -1 )
 
 endlocal
